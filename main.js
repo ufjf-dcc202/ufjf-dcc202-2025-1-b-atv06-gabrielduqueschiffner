ce3dff9eb7ed42bd3493d3f249534cbd05775add
Gabriel Duque Schiffner Oliveira <gabrielduqueschiffner@gmail.com>
2025-08-19 18:35:25 -0300

    cria as 49 celulas do tabuleiro

--- main.js
+++ main.js
@@ -3,10 +3,24 @@
 function criaTabuleiro(){
   const tabuleiro = document.createElement('div');
   tabuleiro.classList.add('tabuleiro');
+
   return tabuleiro;
+}
+
+function criaCelula(){ 
+    const celula = document.createElement('div');
+    celula.classList.add('celula');
+    return celula;
 }
 
 
 
 const tab = criaTabuleiro();
 document.body.appendChild(tab);
+
+  for(let i=0; i<49; i++){
+    const celula = criaCelula();
+    tab.appendChild(celula);
+   
+  }
+
